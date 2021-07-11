74ca3de2f41e6cd87fc7edb8f4c25a22e66fa9f2
Alexandra Nikitina <alexandra.nikitina@gmail.com>
2021-07-11 10:27:33 +0300

    Fixed Tasks 7, 9

--- lesson7/Lesson7Task7.java
+++ lesson7/Lesson7Task7.java
@@ -1,20 +1,32 @@
-package nikitinaalexandra.lesson7;
+package lesson7;
+
+import com.sun.org.apache.xpath.internal.res.XPATHErrorResources_pt_BR;
 
 public class Lesson7Task7 {
     public static void main(String[] args) {
         int numOne = 3;
         int numTwo = 56;
+
+        add(numOne, numTwo);
+        subtract(numOne, numTwo);
+        multiply(numOne, numTwo);
+    }
+    public static void add(int a, int b){
         StringBuilder sbPlus = new StringBuilder();
-        StringBuilder sbMinus = new StringBuilder();
-        StringBuilder sbMult = new StringBuilder();
-        sbPlus.append(numOne).append(" + ").append(numTwo).append(" = ").append(numOne + numTwo);
-        sbMinus.append(numOne).append(" - ").append(numTwo).append(" = ").append(numOne - numTwo);
-        sbMult.append(numOne).append(" * ").append(numTwo).append(" = ").append(numOne * numTwo);
+        sbPlus.append(a).append(" + ").append(b).append(" = ").append(a + b);
         System.out.println(sbPlus);
-        System.out.println(sbMinus);
-        System.out.println(sbMult);
         System.out.println(sbPlus.replace(sbPlus.indexOf("="), sbPlus.indexOf("=") + 1, "равно"));
+    }
+    public static void subtract(int a, int b){
+        StringBuilder sbMinus = new StringBuilder();
+        sbMinus.append(a).append(" - ").append(b).append(" = ").append(a - b);
+        System.out.println(sbMinus);
         System.out.println(sbMinus.replace(sbMinus.indexOf("="), sbMinus.indexOf("=") + 1, "равно"));
+    }
+    public static void multiply(int a, int b){
+        StringBuilder sbMult = new StringBuilder();
+        sbMult.append(a).append(" * ").append(b).append(" = ").append(a * b);
+        System.out.println(sbMult);
         System.out.println(sbMult.replace(sbMult.indexOf("="), sbMult.indexOf("=") + 1, "равно"));
     }
 }
