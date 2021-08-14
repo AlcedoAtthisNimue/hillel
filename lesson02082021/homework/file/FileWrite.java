c4e72f17d4b5798a0392d76760e2d9056e63ce0f
Alexandra Nikitina <alexandra.nikitina@gmail.com>
2021-08-14 11:44:58 +0300

    used default system separator

--- lesson02082021/homework/file/FileWrite.java
+++ lesson02082021/homework/file/FileWrite.java
@@ -7,30 +7,29 @@ import java.nio.file.Paths;
 import java.util.Properties;
 
 public class FileWrite {
-    public static final String WORK_DIRECTORY = "src\\main\\java\\nikitinaalexandra\\lesson02082021\\homework\\file";
+    public static final String WORK_DIRECTORY = "src/main/java/nikitinaalexandra/lesson02082021/homework/file";
     public static final String PROPS_FILE = "data.properties";
     public static final String SAMPLE_DIR = "newdir";
 
     public static void main(String[] args) throws IOException {
-        File propsFile = new File(WORK_DIRECTORY + "\\" + PROPS_FILE);
+        Path propsPath = Paths.get(WORK_DIRECTORY + "/" + PROPS_FILE);
+        File propsFile = new File(propsPath.toString());//returned path string uses the default system separator
         Properties properties = new Properties();
         properties.load(new FileReader(propsFile));
         String sampleFile = properties.getProperty("fileName");
 
-        Path filePath = Paths.get(WORK_DIRECTORY + "\\" + sampleFile);
+        Path filePath = Paths.get(WORK_DIRECTORY + "/" + sampleFile);
         if (!Files.exists(filePath)){
             Files.createFile(filePath);
         }
-        Path dirPath = Paths.get(WORK_DIRECTORY + "\\" + SAMPLE_DIR);
+        Path dirPath = Paths.get(WORK_DIRECTORY + "/" + SAMPLE_DIR);
         if (!Files.exists(dirPath)){
             Files.createDirectory(dirPath);
         }
 
         String text = "Hello!\nMy name is Alexandra Nikitina. I live in Kyiv. I love Java!";
-        FileOutputStream fos = new FileOutputStream(filePath.toString());
         byte[] buffer = text.getBytes();
-        fos.write(buffer, 0, buffer.length);
-        fos.close();
+        Files.write(filePath, buffer);
 
         Path targetPath = Paths.get(dirPath.toString() + "\\" + sampleFile);
         if (Files.exists(targetPath)){
@@ -43,14 +42,5 @@ public class FileWrite {
         while (bufferedReader.ready()) {
             System.out.println(bufferedReader.readLine());
         }
-
-
-
-
-
-
-
-
-
     }
 }
